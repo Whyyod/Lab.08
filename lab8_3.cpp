81d55c18230f26f114c4fb4b2de4422db1c63744
whyyod <Sriintayod@gmail.com>
2024-12-13 00:46:19 +0700

    Complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,67 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+int age , H , B;
+
+cout <<  "Enter your age: ";
+cin >> age;
+    if (age <= 25)
+    {
+        cout << "Enter your height: ";
+        cin >> H;
+        if (H < 100)
+        {
+            cout << "Your character = Chopper" ;
+        }
+        else if (H < 180)
+        {
+            cout << "Your character = Usopp" ;
+
+        }
+        else
+        {
+            cout << "Enter your bounty: ";
+            cin >> B;
+            if (B >1100000000)
+            {
+                cout << "Your character = Zoro";
+            }
+            else
+            {
+                cout << "Your character = Sanji";
+            }
+            
+        }
+        
+    }else if (age <= 60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> B;
+        if (B >= 500000001)
+        {
+            cout << "Your character = Jinbe";
+        }
+        else{
+            cout << "Your character = Franky";
+        }
+
+        
+    } else
+    {
+        cout << "Your character = Brook";
+    }
+    
+    
+}
+
+
+/*cout << "Enter your height: ";
+cin >> H;
+cout << "Enter your bounty: ";
+cin >> B;*/
+
 /*
 "Enter your age: "
 "Enter your height: "
